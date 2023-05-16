5abe54480ba8ad2dcdf7b23eabbbe8c8d26a6cb1
binhtrongg <binhtrong0504@gmail.com>
2023-05-16 13:07:33 +0700

    hw 05

--- spring_boot/hw-05/book_management/src/main/java/com/example/book_management/service/BookBorrowingService.java
+++ spring_boot/hw-05/book_management/src/main/java/com/example/book_management/service/BookBorrowingService.java
@@ -1,5 +1,10 @@
 package com.example.book_management.service;
+import com.example.book_management.model.Book;
 import com.example.book_management.model.BookBorrowing;
+import com.example.book_management.model.BookBorrowingDetail;
+import com.example.book_management.model.Reader;
+import com.example.book_management.statics.BookType;
+import com.example.book_management.statics.ReaderType;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,15 +12,26 @@ import java.util.List;
 @Service
 public class BookBorrowingService {
     static final List<BookBorrowing> bookBorrowings = new ArrayList<>();
+    List<BookBorrowingDetail> bookBorrowingDetails=new ArrayList<>();
     static int auto_id=10000;
 
     public List<BookBorrowing> getBookBorrowings() {
+        creatData();
         return bookBorrowings;
     }
-
     public void saveBookBorrow(BookBorrowing bookBorrowing) {
         ++auto_id;
         bookBorrowing.setId(auto_id);
         bookBorrowings.add(bookBorrowing);
+    }
+    public void creatData(){
+        for (int i = 0; i <5 ; i++) {
+            Reader reader=new Reader(i,"van","van","van",ReaderType.GRADUTE_STUDENT);
+            Book book=new Book(i,"cd","cds",BookType.Electronics_Telecommunication,i);
+            BookBorrowingDetail bookBorrowingDetail=new BookBorrowingDetail(book,i);;
+            bookBorrowingDetails.add(bookBorrowingDetail);
+            BookBorrowing bookBorrowing=new BookBorrowing(i,reader,bookBorrowingDetails);
+            bookBorrowings.add(bookBorrowing);
+        }
     }
 }
